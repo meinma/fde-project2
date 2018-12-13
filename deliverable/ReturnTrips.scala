8dd56f3cfc673f970bd1a3176bece3cd12aa0040
Timo Kersten <kersten@in.tum.de>
2018-12-13 09:53:49 +0100

    Initial commit

---
+++ deliverable/ReturnTrips.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
+
+object ReturnTrips {
+  def compute(trips : Dataset[Row], dist : Double, spark : SparkSession) : Dataset[Row] = {
+
+    import spark.implicits._
+
+    trips
+  }
+}
