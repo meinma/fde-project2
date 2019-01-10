7f507c149b45be3f7cc41b0763f4f7bd8967bfa2
meinma <martin.meinel.7@gmail.com>
2019-01-10 09:11:28 +0100

    Changed order and kept calculating

--- deliverable/ReturnTrips.scala
+++ deliverable/ReturnTrips.scala
@@ -22,7 +22,7 @@ val trips4 = trips.withColumn("lon1_rad", toRadians($"pickup_longitude"))
 
 val trips5 = trips4.select("lon1_rad","lon2_rad","lat1_rad","lat2_rad","tpep_pickup_datetime","tpep_dropoff_datetime")
 
-
+/*
 val distanceBuckets = trips5.withColumn("distanceBucket",floor((
 atan2(
 		sqrt(
@@ -36,8 +36,8 @@ atan2(
 		)
 	) * 6371e3 *2
 
-) / (2 * dist))).cache() 
-//val distanceBuckets = trips5.withColumn("distanceBucket",floor((acos(cos($"lat1_rad")*cos($"lat2_rad")*cos($"lon2_rad" - $"lon1_rad") + sin($"lat1_rad") * sin($"lat2_rad")) *6378137) / (2*dist))).cache()
+) / (2 * dist))).cache() */
+val distanceBuckets = trips5.withColumn("distanceBucket",floor((acos(cos($"lat1_rad")*cos($"lat2_rad")*cos($"lon2_rad" - $"lon1_rad") + sin($"lat1_rad") * sin($"lat2_rad")) *6378137) / (2*dist))).cache()
 
 val distanceNeighbours = distanceBuckets.withColumn("distanceBucket",explode(array($"distanceBucket" - 1,$"distanceBucket",$"distanceBucket" + 1))).cache()
 val pickupBuckets = distanceBuckets.withColumn("pickupBucket",floor((unix_timestamp($"tpep_pickup_datetime") - unix_timestamp(lit("2016-01-01 00:00:00"))) //distanceBuckets
@@ -45,8 +45,7 @@ val pickupBuckets = distanceBuckets.withColumn("pickupBucket",floor((unix_timest
 val dropoffBuckets = distanceNeighbours.withColumn("dropoffBucket",floor((unix_timestamp($"tpep_dropoff_datetime") - unix_timestamp(lit("2016-01-01 00:00:00"))) //distanceNeighbours
  / (8*3600))).cache()
 val pickupNeighbours = pickupBuckets.withColumn("pickupBucket", explode(array($"pickupBucket", $"pickupBucket" + 1))).cache()
-val timeJoin = dropoffBuckets.as("a").join(pickupNeighbours.as("b"),($"a.dropoffBucket" === $"b.pickupBucket")
-	&& ($"a.distanceBucket" === $"b.distanceBucket"))
+val timeJoin = dropoffBuckets.as("a").join(pickupNeighbours.as("b"),($"a.distanceBucket" === $"b.distanceBucket")&&($"a.dropoffBucket" === $"b.pickupBucket"))
 val timeSelect = timeJoin.select($"b.tpep_pickup_datetime",$"a.tpep_dropoff_datetime",$"a.lat2_rad",$"a.lon2_rad",$"a.lat1_rad",$"a.lon1_rad",$"b.lon1_rad",$"b.lat1_rad",$"b.lat2_rad",$"b.lon2_rad")
 val timeFilter = timeSelect.filter(unix_timestamp($"b.tpep_pickup_datetime") > unix_timestamp($"a.tpep_dropoff_datetime"))
 val timeFilter2 = timeFilter.filter(unix_timestamp($"a.tpep_dropoff_datetime") + 8*3600 > unix_timestamp($"b.tpep_pickup_datetime"))
