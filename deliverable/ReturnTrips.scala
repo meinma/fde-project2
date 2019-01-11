668cc138b3b7455d28c392f170d7737f0acb5dd9
meinma <martin.meinel.7@gmail.com>
2019-01-11 16:09:59 +0100

    Verändertes Latitiude Bucketing + Gleiches Longitude Bucketing

--- deliverable/ReturnTrips.scala
+++ deliverable/ReturnTrips.scala
@@ -27,9 +27,9 @@ val trips5 = trips4.select("lon1_rad","lon2_rad","lat1_rad","lat2_rad","tpep_pic
 
 
 
-val pickupLatitudeBucket = trips5.withColumn("pickupLat",floor($"lat1_rad" * (6371e3  / dist)))//.withColumn("pickupLon",floor(ceil($"pickup_longitude"* 111111 * cos(lit("61"))) / (1 * dist)))
-val pickupLatitudeNeighbours = pickupLatitudeBucket.withColumn("pickupLat",explode(array($"pickupLat" - 1,$"pickupLat",$"pickupLat" + 1)))//.withColumn("pickupLon",explode(array($"pickupLon" - 1,$"pickupLon",$"pickupLon" + 1)))
-val dropoffLatitude = trips5.withColumn("dropoffLat",floor($"lat2_rad" * (6371e3 /  dist)))//.withColumn("dropoffLon",floor(ceil($"dropoff_longitude" * 111111 * cos(lit("61"))) / (1 * dist)))
+val pickupLatitudeBucket = trips5.withColumn("pickupLat",floor($"lat1_rad" * (6371e3  / dist))).withColumn("pickupLon",floor(ceil($"pickup_longitude"* 111111 * cos(lit("61"))) / (1 * dist)))
+val pickupLatitudeNeighbours = pickupLatitudeBucket.withColumn("pickupLat",explode(array($"pickupLat" - 1,$"pickupLat",$"pickupLat" + 1))).withColumn("pickupLon",explode(array($"pickupLon" - 1,$"pickupLon",$"pickupLon" + 1)))
+val dropoffLatitude = trips5.withColumn("dropoffLat",floor($"lat2_rad" * (6371e3 /  dist))).withColumn("dropoffLon",floor(ceil($"dropoff_longitude" * 111111 * cos(lit("61"))) / (1 * dist)))
 
 val pickupBuckets = pickupLatitudeNeighbours.withColumn("pickupBucket",floor((unix_timestamp($"tpep_pickup_datetime") - unix_timestamp(lit("2016-01-01 00:00:00"))) //distanceBuckets
 	/ (8*3600)))
@@ -37,7 +37,7 @@ val dropoffBuckets = dropoffLatitude.withColumn("dropoffBucket",floor((unix_time
  / (8*3600)))
 val pickupNeighbours = pickupBuckets.withColumn("pickupBucket", explode(array($"pickupBucket", $"pickupBucket" + 1)))
 val timeJoin = dropoffBuckets.as("a").join(pickupNeighbours.as("b"),($"a.dropoffLat" === $"b.pickupLat")
-	//&& ($"a.dropoffLon" === $"b.pickupLon")
+	&& ($"a.dropoffLon" === $"b.pickupLon")
 	&&($"a.dropoffBucket" === $"b.pickupBucket")
 	&& (unix_timestamp($"a.tpep_dropoff_datetime") < unix_timestamp($"b.tpep_pickup_datetime"))
     && (unix_timestamp($"a.tpep_dropoff_datetime") + 8*3600 > unix_timestamp($"b.tpep_pickup_datetime"))
