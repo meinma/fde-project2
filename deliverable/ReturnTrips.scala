f8cd573da30f5008e1df69c6b5a40cbba4a72abf
meinma <martin.meinel.7@gmail.com>
2019-01-11 14:09:44 +0100

    Changed Calc Distance to Asin from Atan2

--- deliverable/ReturnTrips.scala
+++ deliverable/ReturnTrips.scala
@@ -22,9 +22,9 @@ val trips4 = trips.withColumn("lon1_rad", toRadians($"pickup_longitude"))
 
 
 val trips5 = trips4.select("lon1_rad","lon2_rad","lat1_rad","lat2_rad","tpep_pickup_datetime","tpep_dropoff_datetime","pickup_latitude","pickup_longitude","dropoff_latitude","dropoff_longitude")
-val pickupLatitudeBucket = trips5.withColumn("pickupLat",floor(ceil($"pickup_latitude" * 111111 ) / (1 * dist)))//.withColumn("pickupLon",floor(ceil($"pickup_longitude"* 111111 * cos(lit("61"))) / (1 * dist)))
-val pickupLatitudeNeighbours = pickupLatitudeBucket.withColumn("pickupLat",explode(array($"pickupLat" - 1,$"pickupLat",$"pickupLat" + 1)))//.withColumn("pickupLon",explode(array($"pickupLon" - 1,$"pickupLon",$"pickupLon" + 1)))
-val dropoffLatitude = trips5.withColumn("dropoffLat",floor(ceil($"dropoff_latitude" * 111111 ) / (1 * dist)))//.withColumn("dropoffLon",floor(ceil($"dropoff_longitude" * 111111 * cos(lit("61"))) / (1 * dist)))
+val pickupLatitudeBucket = trips5.withColumn("pickupLat",floor(ceil($"pickup_latitude" * 111111 ) / (1 * dist))).withColumn("pickupLon",floor(ceil($"pickup_longitude"* 111111 * cos(lit("61"))) / (1 * dist)))
+val pickupLatitudeNeighbours = pickupLatitudeBucket.withColumn("pickupLat",explode(array($"pickupLat" - 1,$"pickupLat",$"pickupLat" + 1))).withColumn("pickupLon",explode(array($"pickupLon" - 1,$"pickupLon",$"pickupLon" + 1)))
+val dropoffLatitude = trips5.withColumn("dropoffLat",floor(ceil($"dropoff_latitude" * 111111 ) / (1 * dist))).withColumn("dropoffLon",floor(ceil($"dropoff_longitude" * 111111 * cos(lit("61"))) / (1 * dist)))
 val pickupBuckets = pickupLatitudeNeighbours.withColumn("pickupBucket",floor((unix_timestamp($"tpep_pickup_datetime") - unix_timestamp(lit("2016-01-01 00:00:00"))) //distanceBuckets
 	/ (8*3600)))
 val dropoffBuckets = dropoffLatitude.withColumn("dropoffBucket",floor((unix_timestamp($"tpep_dropoff_datetime") - unix_timestamp(lit("2016-01-01 00:00:00"))) //distanceNeighbours
@@ -32,35 +32,26 @@ val dropoffBuckets = dropoffLatitude.withColumn("dropoffBucket",floor((unix_time
 val pickupNeighbours = pickupBuckets.withColumn("pickupBucket", explode(array($"pickupBucket", $"pickupBucket" + 1)))
 //Join funktioniert und die Spaltenwerte liegen nciht am Join
 val timeJoin = dropoffBuckets.as("a").join(pickupNeighbours.as("b"),($"a.dropoffLat" === $"b.pickupLat")
+	&& ($"a.dropoffLon" === $"b.pickupLon")
 	&&($"a.dropoffBucket" === $"b.pickupBucket")
-	//&& ($"a.dropoffLon" === $"b.pickupLon")
 	//Das hier muss richtig sein
 	&& (unix_timestamp($"a.tpep_dropoff_datetime") < unix_timestamp($"b.tpep_pickup_datetime"))
     && (unix_timestamp($"a.tpep_dropoff_datetime") + 8*3600 > unix_timestamp($"b.tpep_pickup_datetime"))
     //Hier kann nix falsch sein
-    && (lit("2") * (atan2(
+    && (asin(
 		sqrt(
 			sin(($"a.lat2_rad"-$"b.lat1_rad")/2) * sin(($"a.lat2_rad"-$"b.lat1_rad")/2)
 				+ cos($"a.lat2_rad") * cos($"b.lat1_rad")
 				* sin(($"a.lon2_rad"-$"b.lon1_rad")/2) * sin(($"a.lon2_rad"-$"b.lon1_rad")/2)
-		),
-		sqrt(lit("1") - (sin(($"a.lat2_rad"-$"b.lat1_rad")/2) * sin(($"a.lat2_rad"-$"b.lat1_rad")/2)
-				+ cos($"a.lat2_rad") * cos($"b.lat1_rad")
-				* sin(($"a.lon2_rad"-$"b.lon1_rad")/2) * sin(($"a.lon2_rad"-$"b.lon1_rad")/2))
-		)
-	) * 6371000) < dist)
+		)) * 2 * 6371e3 < dist)
 	&& 
-	(lit("2") * (atan2(
+	( asin(
 		sqrt(
 			sin(($"b.lat2_rad"-$"a.lat1_rad")/2) * sin(($"b.lat2_rad"-$"a.lat1_rad")/2)
 				+ cos($"b.lat2_rad") * cos($"a.lat1_rad")
 				* sin(($"b.lon2_rad"-$"a.lon1_rad")/2) * sin(($"b.lon2_rad"-$"a.lon1_rad")/2)
-		),
-		sqrt(lit("1") - (sin(($"b.lat2_rad"-$"a.lat1_rad")/2) * sin(($"b.lat2_rad"-$"a.lat1_rad")/2)
-				+ cos($"b.lat2_rad") * cos($"a.lat1_rad")
-				* sin(($"b.lon2_rad"-$"a.lon1_rad")/2) * sin(($"b.lon2_rad"-$"a.lon1_rad")/2))
 		)
-	) * 6371000) < dist)
+	) * 2 * 6371e3 < dist)
 )
 
 timeJoin
